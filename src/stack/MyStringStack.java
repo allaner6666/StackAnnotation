969aaa750d7002dd163372b06ec39c2ad82ec7b4
allaner6666 <37951217+allaner6666@users.noreply.github.com>
2018-07-17 16:40:50 +0800

    Update MyStringStack.java

--- src/stack/MyStringStack.java
+++ src/stack/MyStringStack.java
@@ -34,8 +34,8 @@ public class MyStringStack {
      */
     public String pop() throws NoSuchElementException {
     	//throw new NoSuchElementException();
-    	String result = v.lastElement();
-    	v.remove(result);
+    	//String result = v.lastElement();
+    	//v.remove(result);
     	System.out.println("hello");
     	return result;
 
